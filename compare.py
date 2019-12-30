bf1c8b72318b2455a3c554b843f90fe010fb3fb3
Kirsty Duffy <ke.duffy1@gmail.com>
2019-12-30 15:09:25 -0600

    Generalize so can now take up to 5 input files and produce comparisons

--- compare.py
+++ compare.py
@@ -1,77 +1,99 @@
-# Make comparison plots from two input files
+# Make comparison plots from input files
+# Note: currently does up to 5 input files. Can be extended to do more if desired
 #
 # A. Mastbaum <mastbaum@uchicago.edu>, 2018/12/20
 
 import sys
 import ROOT
 
-colorv2 = ROOT.kRed
-colorv3 = ROOT.kBlue
+color = [ROOT.kRed,ROOT.kBlue,ROOT.kOrange-3,ROOT.kMagenta+2,ROOT.kGreen+2]
+style = [1,9,2,3,4]
 normalize = False
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('Usage: %s v2.root v3.root' % sys.argv[0])
+    if len(sys.argv) < 5:
+        print('Usage: %s normalize (true/false) input1.root legendtitle1 input2.root legendtitle2 [input3.root legendtitle3 input4.root legendtitle4 input5.root legendtitle5]' % sys.argv[0])
         sys.exit(0)
 
     ROOT.gStyle.SetOptStat(0)
     ROOT.gStyle.SetPalette(ROOT.kBird)
     ROOT.gROOT.SetBatch(True)
 
-    inv2, inv3 = sys.argv[1:3]
+    normalize = sys.argv[1]
+    print(normalize)
 
-    fv2 = ROOT.TFile(inv2)
-    assert(fv2 and fv2.IsOpen())
-    print('GENIEv2: %s' % inv2)
+    i = 0
+    fv = []
+    legendtitle = []
+    for arg in sys.argv:
+        if i<2:
+            i = i+1
+            continue
+        if i%2 == 0:
+            fv.append(ROOT.TFile(arg))
+            print('Adding to comparison: %s' % arg)
+        else:
+            legendtitle.append(arg)
+            print(' -- legend title: %s' % arg)
+        i = i+1
 
-    fv3 = ROOT.TFile(inv3)
-    assert(fv3 and fv3.IsOpen())
-    print('GENIEv3: %s' % inv3)
+    for i in range(len(fv)):
+        assert(fv[i] and fv[i].IsOpen())
 
-    for k in fv2.GetListOfKeys():
+    assert(len(fv)==len(legendtitle))
+
+    for k in fv[0].GetListOfKeys():
+        ov = []
         print('OBJ %s' % k.GetName())
-        ov2 = fv2.Get(k.GetName())
-        ov3 = fv3.Get(k.GetName())
-        assert(ov2 and ov3)
+        for i in range(len(fv)):
+            ov.append(fv[i].Get(k.GetName()))
+            assert(ov[i])
+            ov[i].SetLineColor(color[i])
+            ov[i].SetLineWidth(2)
+            ov[i].SetLineStyle(style[i])
 
-        ov2.SetLineColor(colorv2)
-        ov2.SetLineWidth(2)
+            if normalize and ov[i].Integral()!=0:
+                # print('normalizing')
+                ov[i].Scale(1.0/ov[i].Integral())
 
-        ov3.SetLineColor(colorv3)
-        ov3.SetLineWidth(2)
-
-        if normalize:
-            ov2.Scale(1.0 / ov2.Integral())
-            ov3.Scale(1.0 / ov3.Integral())
 
         c = ROOT.TCanvas('c', '', 500, 500)
         c.SetLeftMargin(0.15)
 
-        if ov2.IsA() == ROOT.TH1F.Class():
-            ov2.Draw('e1')
-            ov3.Draw('e1 same')
+        if ov[0].IsA() == ROOT.TH1F.Class():
+            ov[0].Draw('e1')
+            for i in range(len(ov)):
+                ov[i].Draw('e1 same')
 
-            ymax = 1.1 * max(ov2.GetMaximum(), ov3.GetMaximum())
-            ov2.GetYaxis().SetRangeUser(0, ymax)
+            ymax = ov[0].GetMaximum()
+            for i in range(len(ov)):
+                if ov[i].GetMaximum() > ymax:
+                    ymax = ov[i].GetMaximum()
+            ymax *= 1.1
+
+            ov[0].GetYaxis().SetRangeUser(0, ymax)
 
             l = ROOT.TLegend(0.5, 0.65, 0.88, 0.88)
             l.SetBorderSize(0)
             l.SetFillColor(0)
             l.SetFillStyle(0)
-            l.AddEntry(ov2, 'GENIE v2.12.10')
-            l.AddEntry(ov3, 'GENIE v3.00.01')
+            for i in range(len(ov)):
+                l.AddEntry(ov[i],legendtitle[i])
             l.Draw()
 
-        elif ov2.IsA() == ROOT.TH2F.Class():
-            c.SetRightMargin(0.17)
-            ov3.Divide(ov2)
-            ov3.GetZaxis().SetTitle('Ratio GENIEv3/GENIEv2')
-            ov3.Draw('colz')
-            ov3.GetZaxis().SetRangeUser(0, 2)
-            ov3.GetZaxis().SetTitleOffset(1.15)
+            plotname = '_'.join(['cmp'] + k.GetName().split('_')[1:]) + '.pdf'
+            c.SaveAs(plotname)
 
-        plotname = '_'.join(['cmp'] + k.GetName().split('_')[1:]) + '.pdf'
-        c.SaveAs(plotname)
+        elif ov[0].IsA() == ROOT.TH2F.Class():
+            c.SetRightMargin(0.17)
+            for i in range(len(ov)):
+                ov[i].Divide(ov[0])
+                ov[i].GetZaxis().SetTitle('Ratio'+legendtitle[i]+'/'+legendtitle[0])
+                ov[i].Draw('colz')
+                ov[i].GetZaxis().SetRangeUser(0,2)
+                ov[i].GetZaxis().SetTitleOffset(1.15)
+
+                plotname = '_'.join(['cmp'] + k.GetName().split('_')[1:]) + '.pdf'
+                c.SaveAs(plotname)
 
         del c
-
